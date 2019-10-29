d3e85d081cca6c47453f9470e2c245061033a801
patoarvizu <patoarvizu@gmail.com>
2019-10-29 17:41:02 -0400

    Cleanup comments and add return statements

--- pkg/controller/serviceaccount/serviceaccount_controller.go
+++ pkg/controller/serviceaccount/serviceaccount_controller.go
@@ -9,7 +9,6 @@ import (
 	bankvaultsv1alpha1 "github.com/banzaicloud/bank-vaults/operator/pkg/apis/vault/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -25,31 +24,20 @@ const defaultPolicyTemplate = "path \"secret/{{ .Name }}\" { capabilities = [\"r
 
 var log = logf.Log.WithName("controller_serviceaccount")
 
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
-
-// Add creates a new ServiceAccount Controller and adds it to the Manager. The Manager will set fields on the Controller
-// and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
 }
 
-// newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 	return &ReconcileServiceAccount{client: mgr.GetClient(), scheme: mgr.GetScheme()}
 }
 
-// add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
 	c, err := controller.New("serviceaccount-controller", mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
 
-	// Watch for changes to primary resource ServiceAccount
 	err = c.Watch(&source.Kind{Type: &corev1.ServiceAccount{}}, &handler.EnqueueRequestForObject{})
 	if err != nil {
 		return err
@@ -82,17 +70,16 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 			}),
 		},
 	)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
-// blank assignment to verify that ReconcileServiceAccount implements reconcile.Reconciler
 var _ reconcile.Reconciler = &ReconcileServiceAccount{}
 
-// ReconcileServiceAccount reconciles a ServiceAccount object
 type ReconcileServiceAccount struct {
-	// This client, initialized using mgr.Client() above, is a split client
-	// that reads objects from the cache and writes to the apiserver
 	client client.Client
 	scheme *runtime.Scheme
 }
@@ -124,28 +111,16 @@ type policyTemplateInput struct {
 	Namespace string
 }
 
-// Reconcile reads that state of the cluster for a ServiceAccount object and makes changes based on the state read
-// and what is in the ServiceAccount.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
-// a Pod as an example
-// Note:
-// The Controller will requeue the Request to be processed again if the returned error is non-nil or
-// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
 func (r *ReconcileServiceAccount) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
 	reqLogger.Info("Reconciling ServiceAccount")
 
-	// Fetch the ServiceAccount instance
 	instance := &corev1.ServiceAccount{}
 	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
 			return reconcile.Result{}, nil
 		}
-		// Error reading the object - requeue the request.
 		return reconcile.Result{}, err
 	}
 
@@ -158,12 +133,14 @@ func (r *ReconcileServiceAccount) Reconcile(request reconcile.Request) (reconcil
 	err = r.client.Get(context.TODO(), types.NamespacedName{Name: "vault", Namespace: "vault"}, vaultConfig)
 	if err != nil {
 		reqLogger.Error(err, "Error getting Vault configuration")
+		return reconcile.Result{}, err
 	} else {
 		var bvConfig bankVaultsConfig
 		jsonData, _ := json.Marshal(vaultConfig.Spec.ExternalConfig)
 		err = json.Unmarshal(jsonData, &bvConfig)
 		if err != nil {
 			reqLogger.Error(err, "Error unmarshaling config")
+			return reconcile.Result{}, err
 		} else {
 			configMap := &corev1.ConfigMap{}
 			err = r.client.Get(context.TODO(), types.NamespacedName{Name: "vault-dynamic-configuration", Namespace: "vault"}, configMap)
@@ -204,6 +181,7 @@ func (r *ReconcileServiceAccount) Reconcile(request reconcile.Request) (reconcil
 			err = json.Unmarshal(configJsonData, &vaultConfig.Spec.ExternalConfig)
 			if err != nil {
 				reqLogger.Error(err, "Error unmarshaling updated config")
+				return reconcile.Result{}, err
 			} else {
 				r.client.Update(context.TODO(), vaultConfig)
 			}
@@ -238,27 +216,4 @@ func roleExists(roles []role, name string) bool {
 		}
 	}
 	return false
-}
-
-// newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *corev1.ServiceAccount) *corev1.Pod {
-	labels := map[string]string{
-		"app": cr.Name,
-	}
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name + "-pod",
-			Namespace: cr.Namespace,
-			Labels:    labels,
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "busybox",
-					Command: []string{"sleep", "3600"},
-				},
-			},
-		},
-	}
 }
